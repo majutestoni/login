382351c83d2e98c3c2e4ebd29bb2eb437635c963
majutestoni <majuliatestoni@gmail.com>
2022-02-16 10:45:38 -0300

    alterando script2 para validação

--- script2.js
+++ script2.js
@@ -1,25 +1,95 @@
 let nome = document.querySelector('#nome');
 let labelNome = document.querySelector('#labelNome');
+let validNome = false;//Aletara maneira de validar cadastro(futuramente)
 
 let usuario = document.querySelector('#usuario');
 let labelUsuario = document.querySelector('#labelUsuario');
+let validUsuario = false;
 
 let senha = document.querySelector('#senha');
 let labelSenha = document.querySelector('#labelSenha');
+let validSenha = false;
 
 let confirmarSenha = document.querySelector('#confirmarSenha');
 let labelConfirmar = document.querySelector('#labelConfirmar');
+let validConfirmar = false;
+
+let mensagemErro = document.getElementById('mensagemErro')
+let mensagemSucesso = document.getElementById('mensagemSucesso')
+
 
 nome.addEventListener('keyup', () => {
-    if (nome.ariaValueMax <= 2) {
+    if (nome.value.length <= 2) {
         labelNome.setAttribute('style', 'color: red');
-        labelNome.innerHTML= 'Nome *Insira no minimo 3 caracteres'
+        labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
+        labelNome.setAttribute('style', 'border-color: red');
+        validNome = false;
     } else {
         labelNome.setAttribute('style', 'color: green');
-        labelNome.innerHTML= 'Nome'
+        labelNome.innerHTML = 'Nome'
+        validNome = true;
+    }
+})
+
+usuario.addEventListener('keyup', () => {
+    if (usuario.value.length <= 2) {
+        labelUsuario.setAttribute('style', 'color: red');
+        labelUsuario.innerHTML = 'Usuario *Insira no minimo 3 caracteres'
+        labelUsuario.setAttribute('style', 'border-color: red');
+        validUsuario = false;
+    } else {
+        labelUsuario.setAttribute('style', 'color: green');
+        labelUsuario.innerHTML = 'Usuario'
+        validUsuario = true;
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <= 2) {
+        labelSenha.setAttribute('style', 'color: red');
+        labelSenha.innerHTML = 'Senha *Insira no minimo 3 caracteres'
+        labelSenha.setAttribute('style', 'border-color: red');
+        validSenha = false;
+    } else {
+        labelSenha.setAttribute('style', 'color: green');
+        labelSenha.innerHTML = 'Senha'
+        validSenha = true;
+    }
+})
+
+confirmarSenha.addEventListener('keyup', () => {
+    if (senha.value != confirmarSenha.value) {
+        labelConfirmar.setAttribute('style', 'color: red');
+        labelConfirmar.innerHTML = 'Confirmar Senha *As senhas não conferem'
+        labelConfirmar.setAttribute('style', 'border-color: red');
+        validConfirmar = false;
+    } else {
+        labelConfirmar.setAttribute('style', 'color: green');
+        labelConfirmar.innerHTML = 'Confirmar Senha'
+        validConfirmar = true;
     }
 })
 
 function cadastrar() {
+    if (validConfirmar && validSenha && validNome && validUsuario) {
+        let listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios') || '[]');
 
+        listaUsuarios.push(
+            {
+                nomeCadastrado: nome.value,
+                usuarioCadastrador: usuario.value,
+                senhaCadastrada: senha.value
+
+            }
+        )
+        localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
+
+
+        mensagemSucesso.setAttribute('style', 'display:block');
+        mensagemSucesso.innerHTML = '<strong>Cadastrando usuario...</strong>';
+        window.location.href = ('index.html')
+    } else {
+        mensagemErro.setAttribute('style', 'display:block')
+        mensagemErro.innerHTML = '<strong>Faltam campos a serem preenchidos!</strong>'
+    }
 }
